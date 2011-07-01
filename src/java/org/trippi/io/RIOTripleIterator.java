fcrepo3/trippi

commit 4ef40ea22b4a6b359de73c4b4d8b5a9fe432a594

FCREPO-946: Rebuilder does not terminate
This commit addresses the issue by moving back to a thread-per-parse model.
While less efficient than the change introduced in commit 62b0725f7abc1d751756,
it does not require explicit cleanup in order for the JVM to exit naturally.

https://jira.duraspace.org/browse/FCREPO-946

src/java/org/trippi/io/RIOTripleIterator.java
@@ -8,8 +8,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +37,6 @@ public class RIOTripleIterator extends TripleIterator
     private static final Logger logger =
         LoggerFactory.getLogger(RIOTripleIterator.class.getName());
     private static final Triple FINISHED = new AbstractTriple(){};
-    private static final Executor EXECUTOR = Executors.newCachedThreadPool();
     private InputStream m_in;
     private RDFParser m_parser;
     private String m_baseURI;
@@ -72,7 +69,7 @@ public class RIOTripleIterator extends TripleIterator
         if (logger.isDebugEnabled()) {
         	logger.debug("Starting parse thread");
         }
-        EXECUTOR.execute(this);
+        new Thread(this).run();
         m_next = getNext();
     }
 

commit 80281d4513edf654d17f96ac3cdad52e9090d54f

FCREPO-946: Rebuilder does not terminate (Part II: Fixing the Fix)
This commit fixes the error introduced in 4ef40ea22b4a6b359de7.
Rather than calling Thread.run, this now correctly calls Thread.start,
which spins off another thread, thus ensuring that the careful coordination
between the triple putter and getter threads doesn't result in a deadlock :)

Also upped version to 1.5.3

https://jira.duraspace.org/browse/FCREPO-946

src/java/org/trippi/Trippi.properties
@@ -1,2 +1,2 @@
-trippi.version=1.5.2
+trippi.version=1.5.3
 buildDate=@buildDate@

src/java/org/trippi/io/RIOTripleIterator.java
@@ -69,7 +69,7 @@ public class RIOTripleIterator extends TripleIterator
         if (logger.isDebugEnabled()) {
         	logger.debug("Starting parse thread");
         }
-        new Thread(this).run();
+        new Thread(this).start();
         m_next = getNext();
     }
 